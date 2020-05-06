9a4dd02cea0a845126cdf88844b385ad9710b7df
yn09 <yn09@LAPTOP-7HRTPINS>
2020-05-06 15:45:36 +0900

    JSP

---
+++ jspstudy2/src/com/test/jsp/RedirectServlet.java
@@ -0,0 +1,28 @@
+package com.test.jsp;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/RedirectServlet")
+public class RedirectServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProc(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProc(request, response);
+	}
+	protected void doProc(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		request.setAttribute("data", "데이터");
+		response.sendRedirect("redirect.jsp");
+	}
+
+}
